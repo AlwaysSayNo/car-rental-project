b454609d72e0e1845ae94008973bdf5c4e8e68ec
naconechnoi <anast.sad@gmail.com>
2022-05-22 12:41:32 +0300

    Added beans in WebConfig for Internationalization.

--- src/main/java/com/epam/nazar/grinko/configs/WebConfig.java
+++ src/main/java/com/epam/nazar/grinko/configs/WebConfig.java
@@ -2,16 +2,20 @@ package com.epam.nazar.grinko.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.util.Locale;
 
 @Configuration
 @ComponentScan("com.epam.nazar.grinko")
@@ -34,15 +38,23 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     // configures access to non-static resources (like path`s prefix, suffix, encoding, ...)
+    // SpringResourceTemplateResolver automatically integrates with Spring's own
+    // resource resolution infrastructure, which is highly recommended.
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
+        // TemplateMode == HTML and Template cache == true
+        // are the default values, added here for the sake of clarity.
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(true);
         return templateResolver;
     }
 
+    // SpringTemplateEngine automatically applies SpringStandardDialect and
+    // enables Spring's own MessageSource message resolution mechanisms.
     @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
@@ -56,9 +68,43 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
+        // 'order' and 'viewNames' are optional
+        resolver.setOrder(1);
+        resolver.setViewNames(new String[] {".html"});
         registry.viewResolver(resolver);
     }
 
     // Configure locale Notepad.WebConfig(messageSource, addInterceptors, localeResolver)
+    // messageResolver
 
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource resource = new ResourceBundleMessageSource();
+        resource.setBasename("locale");
+        return resource;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
+
+    @Bean
+    public CookieLocaleResolver localeResolver(){
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        localeResolver.setCookieName("my-locale-cookie");
+        localeResolver.setCookieMaxAge(3600);
+        return localeResolver;
+    }
 }
