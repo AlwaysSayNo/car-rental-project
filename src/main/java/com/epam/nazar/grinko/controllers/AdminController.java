6e49f295baa132030b4968c6a2a4a3a0dd17f432
AlwaysSayNo <allbitxxx@gmail.com>
2022-05-29 13:09:54 +0300

    Implemented web site API for endpoints /car-rental-service/admin/cars and /car-rental-service/admin/cars/new. Implemented show-cars.html and add-new-car.html view files.

--- src/main/java/com/epam/nazar/grinko/controllers/AdminController.java
+++ src/main/java/com/epam/nazar/grinko/controllers/AdminController.java
@@ -1,26 +1,33 @@
 package com.epam.nazar.grinko.controllers;
 
 import com.epam.nazar.grinko.domians.Car;
+import com.epam.nazar.grinko.domians.CarBrand;
+import com.epam.nazar.grinko.domians.CarColor;
+import com.epam.nazar.grinko.domians.helpers.CarSegment;
+import com.epam.nazar.grinko.dto.CarDto;
+import com.epam.nazar.grinko.services.CarBrandService;
+import com.epam.nazar.grinko.services.CarColorService;
 import com.epam.nazar.grinko.services.CarService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/car-rental-service/admin")
+@AllArgsConstructor
 public class AdminController {
 
     private final CarService carService;
+    private final CarBrandService carBrandService;
+    private final CarColorService carColorService;
 
-    public AdminController(CarService carService) {
-        this.carService = carService;
-    }
-
-    // pagination
+    // ? pagination
     @GetMapping("/cars")
     public String showAllCars(Model model){
         model.addAttribute("cars", carService.getAllCars());
@@ -28,38 +35,38 @@ public class AdminController {
     }
 
     @GetMapping("/cars/new")
-    public String showCarCreationForm(Model model, Car car){
-        model.addAttribute("car", car);
+    public String showCarCreationForm(Model model, CarDto carDto){
+        model.addAttribute("carDto", carDto);
+        //model.addAttribute("carNumberAlreadyExistsError", false);
         return "admin/add-new-car";
     }
 
-    // Валидация формы на стороне клиента.
-    @PostMapping("/cars")
-    public String addNewCar(Model model, @ModelAttribute("car") Car car){
-        boolean isAdded = carService.addNewCar(car);
-        if(!isAdded) {
-            model.addAttribute("alreadyExistsError", true);
+    // Валидация формы только на стороне клиента.
+    @PostMapping("/cars/new")
+    public String addNewCar(@ModelAttribute("carDto") CarDto carDto, Model model){
+        if(carService.existsCarByNumber(carDto.getNumber())){
+            model.addAttribute("carNumberAlreadyExistsError", true);
+            model.addAttribute("carDto", new CarDto());
             return "admin/add-new-car";
         }
-        return "redirect:/cars";
-    }
 
-    @GetMapping("/cars/{id}")
-    public String showCarWithId(Model model, @PathVariable long id){
-        Optional<Car> carOptional = carService.getCarById(id);
-        if(carOptional.isPresent()) {
-            model.addAttribute("car", carOptional.get());
-            return "admin/show-car-with-id";
+        if(!carColorService.carColorExists(carDto.getColor())) {
+            CarColor color = new CarColor().setColor(carDto.getColor());
+            carColorService.addNewColor(color);
         }
-        // Exception handler
-        return "errors/404";
+        if(!carBrandService.carBrandExists(carDto.getBrand())){
+            CarBrand brand = new CarBrand().setBrand(carDto.getBrand());
+            carBrandService.addNewBrand(brand);
+        }
+
+        Car car = carService.convertCarDtoToCar(carDto);
+        carService.addNewCar(car);
+
+        return "redirect:/car-rental-service/admin/cars";
     }
 
-    @DeleteMapping("cars/{id}")
+    @DeleteMapping("cars/{id}/edit")
     public String deleteCarWithId(@PathVariable long id){
-        boolean isDeleted = carService.deleteCarById(id);
-        if(isDeleted) return "redirect:/cars";
-        // Exception handler
         return "errors/404";
     }
 
@@ -72,6 +79,18 @@ public class AdminController {
         return "";
     }
 
+    @ModelAttribute("brands")
+    private List<CarBrand> addBrandsAttribute(){
+        return carBrandService.getAllCarBrands();
+    }
 
+    @ModelAttribute("colors")
+    private List<CarColor> addColorsAttribute(){
+        return carColorService.getAllCarColors();
+    }
 
+    @ModelAttribute("segments")
+    private List<CarSegment> addSegmentsAttribute(){
+        return Arrays.asList(CarSegment.values());
+    }
 }
