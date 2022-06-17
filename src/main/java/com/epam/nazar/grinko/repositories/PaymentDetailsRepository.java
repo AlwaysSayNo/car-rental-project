1912313cdec886441da29dddf3fe160b56b1b956
AlwaysSayNo <allbitxxx@gmail.com>
2022-06-17 17:44:21 +0300

    Added methods for working with payments in the service and repository class.

--- src/main/java/com/epam/nazar/grinko/repositories/PaymentDetailsRepository.java
+++ src/main/java/com/epam/nazar/grinko/repositories/PaymentDetailsRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentDetailsRepository extends JpaRepository<PaymentDetails, Long> {
+
+    boolean existsByPassportNumber(String passportNumber);
+
 }
--- src/main/java/com/epam/nazar/grinko/services/PaymentDetailsService.java
+++ src/main/java/com/epam/nazar/grinko/services/PaymentDetailsService.java
@@ -1,12 +1,13 @@
 package com.epam.nazar.grinko.services;
 
-import com.epam.nazar.grinko.domians.Bill;
+import com.epam.nazar.grinko.domians.Breakdown;
 import com.epam.nazar.grinko.domians.PaymentDetails;
-import com.epam.nazar.grinko.dto.BillDto;
 import com.epam.nazar.grinko.dto.PaymentDetailsDto;
 import com.epam.nazar.grinko.repositories.PaymentDetailsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -14,12 +15,26 @@ public class PaymentDetailsService {
 
     private PaymentDetailsRepository paymentDetailsRepository;
 
-    public PaymentDetails convertPaymentDetailsDtoToPaymentDetails(PaymentDetailsDto paymentDetailsDto){
+    public PaymentDetails mapToObject(PaymentDetailsDto paymentDetailsDto){
         return new PaymentDetails().setFirstName(paymentDetailsDto.getFirstName())
                 .setLastName(paymentDetailsDto.getLastName())
                 .setDateOfBirth(paymentDetailsDto.getDateOfBirth())
                 .setPassportNumber(paymentDetailsDto.getPassportNumber());
     }
 
+    public void addBreakdown(PaymentDetails paymentDetails, Breakdown breakdown){
+        Set<Breakdown> breakdowns = paymentDetails.getBreakdowns();
+        breakdowns.add(breakdown);
+        paymentDetails.setBreakdowns(breakdowns);
+    }
+
+    public void saveIfNotExists(PaymentDetails paymentDetails){
+        if(!existsByPassportNumber(paymentDetails.getPassportNumber()))
+            paymentDetailsRepository.save(paymentDetails);
+    }
+
+    public boolean existsByPassportNumber(String passportNumber){
+        return paymentDetailsRepository.existsByPassportNumber(passportNumber);
+    }
 
 }
