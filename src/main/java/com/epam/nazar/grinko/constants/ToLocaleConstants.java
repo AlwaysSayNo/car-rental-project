453337706a9347819cd1ceb45d049766ddaeb240
AlwaysSayNo <allbitxxx@gmail.com>
2022-06-26 13:47:07 +0300

    Added TranslationService for translating messages on server.

---
+++ src/main/java/com/epam/nazar/grinko/constants/ToLocaleConstants.java
@@ -0,0 +1,7 @@
+package com.epam.nazar.grinko.constants;
+
+public interface ToLocaleConstants {
+
+    String DEFAULT_CANCELLATION_MESSAGE = "default.cancellation.message";
+
+}
---
+++ src/main/java/com/epam/nazar/grinko/services/TranslationService.java
@@ -0,0 +1,21 @@
+package com.epam.nazar.grinko.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+@AllArgsConstructor
+public class TranslationService {
+
+    private ResourceBundleMessageSource messageSource;
+
+    public String toLocale(String code) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(code, null, locale);
+    }
+
+}
